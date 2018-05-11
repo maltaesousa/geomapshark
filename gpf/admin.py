cd62cedd70f360e5becea253ac0b1248b33f3f29
maltaesousa <sos@ylb.ch>
2018-05-11 15:16:56 +0200

    register all models

--- gpf/admin.py
+++ gpf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib.gis import admin
-# Register your models here.
-from .models import PermisFouille
+from .models import * 
 
 admin.site.register(PermisFouille, admin.OSMGeoAdmin)
+admin.site.register(Demande)
+admin.site.register(Intervenant)
+admin.site.register(Service)
+admin.site.register(TypeEmplacement)
+admin.site.register(Validation)
