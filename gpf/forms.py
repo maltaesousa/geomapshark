3de127b7ac2beb4b732f93096bcc0227e29ea551
maltaesousa <sos@ylb.ch>
2018-05-24 16:51:26 +0200

    add model based form

--- gpf/forms.py
+++ gpf/forms.py
@@ -1,13 +1,33 @@
-from django.forms import modelformset_factory, ModelForm
 from django.contrib.gis import forms
+from .models import PermitRequest, Actor, Validation
 
-class SitOpenLayersWidget(forms.OpenLayersWidget):
+class SitOpenLayersWidget(forms.OSMWidget):
     class Media:
-        js = ('openlayers.js')
+        js = ('ol-debug.js',)
 
-class PermisFouilleForm(forms.Form):
-    geom = forms.PointField(widget=
-        forms.OpenLayersWidget(attrs={
-            'map_width': 800,
-            'map_height': 500,
-            'map_srid': 2056}))
+class PermitRequestForm(forms.ModelForm):
+    class Meta:
+        model = PermitRequest
+        fields = '__all__'
+        widgets = {
+            'geom': SitOpenLayersWidget(attrs={
+                'map_width': 800,
+                'map_height': 500,
+                'map_srid': 2056
+                }
+            )
+        }
+
+class ActorForm(forms.ModelForm):
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+class ValidationForm(forms.ModelForm):
+    class Meta:
+        model = Validation
+        fields = '__all__'
+
+
+ValidationFormSet = forms.inlineformset_factory(
+    PermitRequest, Validation, form=ValidationForm, extra=1)
--- gpf/views.py
+++ gpf/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect,HttpResponse
 from .models import PermitRequest
-from .forms import PermisFouilleForm
+from .forms import PermitRequestForm
 
 def index(request):
-    return HttpResponse("Application de Gestion des Permis de Fouille")
-
-# def edit(request):
-#     fouille = get_object_or_404(PermitRequest, pk=12)
-#     return render(request, 'gpf/edit.html', {'fouille': fouille})
+    return render(request, 'gpf/index.html', {})
 
 def edit(request):
     # fouille = get_object_or_404(PermitRequest, pk=12)
     # PermisFouilleFormSet = modelformset_factory(PermitRequest, fields=('objectid', 'geom'))
 
     if request.method == 'POST':
-        formset = PermisFouilleForm(request.POST, request.FILES)
+        formset = PermitRequestForm(request.POST, request.FILES)
         if formset.is_valid():
             formset.save()
             # do something.
     else:
-        formset = PermisFouilleForm()
+        formset = PermitRequestForm()
     return render(request, 'gpf/edit.html', {'formset': formset})
