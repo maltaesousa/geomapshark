0dff4d053ce874a15e87d625128ed857336ce361
monodo <olivier.monod@gmail.com>
2018-05-29 17:31:56 +0200

    setup authetifications, templates to be defined

--- geomapshark/urls.py
+++ geomapshark/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('gpf/', include('gpf.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/profile/', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- gpf/views.py
+++ gpf/views.py
@@ -5,15 +5,16 @@ from .models import PermitRequest
 from .forms import PermitRequestForm
 
 from django.views.generic.list import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@login_required
 def index(request):
     return render(request, 'gpf/index.html', {})
 
-
+@login_required
 def edit(request):
-
-    # fouille = get_object_or_404(PermitRequest, pk=12)
 
     if request.method == 'POST':
         formset = PermitRequestForm(request.POST, request.FILES)
@@ -24,11 +25,11 @@ def edit(request):
         formset = PermitRequestForm()
     return render(request, 'gpf/edit.html', {'formset': formset})
 
+@method_decorator(login_required, name='dispatch')
 class PermitListView(ListView):
 
     model = PermitRequest
     paginate_by = 15  # if pagination is desired
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
