60aeda61138d32357cf54b63f599d5f198b55a1b
monodo <olivier.monod@gmail.com>
2018-05-29 14:44:55 +0200

    add basic listView - WIP

--- gpf/views.py
+++ gpf/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.http import HttpResponseRedirect,HttpResponse
 from .models import PermitRequest
 from .forms import PermitRequestForm
 
+from django.views.generic.list import ListView
+
+
 def index(request):
     return render(request, 'gpf/index.html', {})
 
+
 def edit(request):
+
     # fouille = get_object_or_404(PermitRequest, pk=12)
-    # PermisFouilleFormSet = modelformset_factory(PermitRequest, fields=('objectid', 'geom'))
 
     if request.method == 'POST':
         formset = PermitRequestForm(request.POST, request.FILES)
@@ -18,3 +23,13 @@ def edit(request):
     else:
         formset = PermitRequestForm()
     return render(request, 'gpf/edit.html', {'formset': formset})
+
+class PermitListView(ListView):
+
+    model = PermitRequest
+    paginate_by = 15  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
