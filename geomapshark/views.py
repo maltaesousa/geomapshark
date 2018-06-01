20ec9a7cd791b45bca2ec732f2f1dfd88720b954
maltaesousa <sos@ylb.ch>
2018-06-01 15:20:14 +0200

    ignore django_filters

--- geomapshark/views.py
+++ geomapshark/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     apps = []
     for app in settings.INSTALLED_APPS:
-        if not app.startswith('django.contrib') and not app.startswith('boot'):
+        if not app.startswith('django.contrib') and \
+           app not in ['bootstrap4', 'django_filters']:
             apps.append(app)
     context = { 'apps': apps }
     return render(request, 'geomapshark/index.html', context)
