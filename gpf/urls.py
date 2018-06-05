48498c8bd24ac52ca2fcdf3981b12a27d4c0daa1
maltaesousa <sos@ylb.ch>
2018-06-05 16:54:58 +0200

    change routes: add actor comes first and calls add permit request

--- gpf/urls.py
+++ gpf/urls.py
@@ -4,10 +4,9 @@ from . import views
 app_name = 'gpf'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('actor/add', views.actorAddPopup, name='actor_add'),
-    path('actor/<int:pk>/change', views.actorChangePopup, name='ActorChange'),
-    path('actor/ajax/get_actor_id', views.get_actor_id, name='get_actor_id'),
-    path('add/', views.permitRequestAdd, name='permit-request-add'),
+    path('add/', views.actorAdd, name='actor_add'),
+    path('actor/<int:pk>/change', views.actorChange, name='ActorChange'),
+    path('add/<int:project_owner_id>/', views.permitRequestAdd, name='permit-request-add'),
     path('change/', views.permitRequestChange, name='permit-request-change'),
     path('listpermit/', views.listpermit, name='list-permit'),
     path('permitdetail/<int:pk>', views.permitdetail, name='permitdetail')
