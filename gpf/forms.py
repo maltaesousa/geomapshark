0fdd5637b4486363a8f78a6befd8f88d37c4aa64
maltaesousa <sos@ylb.ch>
2018-06-05 16:54:19 +0200

    delete "AddAnother" , add template_name to widget

--- gpf/forms.py
+++ gpf/forms.py
@@ -8,37 +8,14 @@ from .models import PermitRequest, Actor, Validation
 
 
 class SitOpenLayersWidget(forms.OSMWidget):
+    template_name = 'openlayers/openlayers.html'
     @property
     def media(self):
         return forms.Media(
             css={'all': ('libs/js/openlayers/ol.css',)},
             js=('libs/js/openlayers/ol-debug.js',
-                'customWidgets/sitMapWidget/sitMapWidget.js'))
-
-
-class RelatedFieldWidgetCanAdd(widgets.Select):
-    def __init__(self, related_model, related_url=None, *args, **kw):
-        super(RelatedFieldWidgetCanAdd, self).__init__(*args, **kw)
-
-        if not related_url:
-            rel_to = related_model
-            info = (rel_to._meta.app_label, rel_to._meta.object_name.lower())
-            related_url = 'admin:%s_%s_add' % info
-
-        # Be careful that here "reverse" is not allowed
-        self.related_url = related_url
-
-    def render(self, name, value, *args, **kwargs):
-        self.related_url = reverse(self.related_url)
-        output = [super(RelatedFieldWidgetCanAdd, self).render(name, value, *args, **kwargs)]
-        output.append(
-            '<a href="%s" class="add-another" id="add_id_%s" \
-            onclick="return showAddAnotherPopup(this);">Add Another</a>' % \
-            (self.related_url, name))
-        output.append(
-            '<a id="edit_actor" style="cursor: pointer; cursor: hand;">Edit</a>'
-        )
-        return mark_safe(''.join(output))
+                'customWidgets/sitMapWidget/sitMapWidget.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.4.4/proj4-src.js'))
 
 
 class AddPermitRequestForm(forms.ModelForm):
@@ -47,7 +24,7 @@ class AddPermitRequestForm(forms.ModelForm):
         fields = [
             'geom', 'description', 'date_start', 'date_end', 'sitetype',
             'length', 'width', 'road_marking_damaged', 'is_green_area',
-            'project_owner', 'invoice_to'
+            'invoice_to'
         ]
         widgets = {
             'geom': SitOpenLayersWidget(attrs={
@@ -58,7 +35,6 @@ class AddPermitRequestForm(forms.ModelForm):
             'date_start': forms.SelectDateWidget(),
             'date_end': forms.SelectDateWidget(),
             'date_effective_end': forms.SelectDateWidget(),
-            'project_owner': RelatedFieldWidgetCanAdd(Actor, related_url='gpf:actor_add')
         }
 
 
