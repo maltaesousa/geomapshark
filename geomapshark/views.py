175bf0c44e0b3932fa4df082d24e53799c856c6b
maltaesousa <sos@ylb.ch>
2018-06-06 08:50:19 +0200

    filter apps on index

--- geomapshark/views.py
+++ geomapshark/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     apps = []
     for app in settings.INSTALLED_APPS:
-        if not app.startswith('django.contrib') and \
-           app not in ['bootstrap4', 'django_filters']:
+        if not app.startswith('django') and \
+           not app.startswith('bootstrap'):
             apps.append(app)
     context = { 'apps': apps }
     return render(request, 'geomapshark/index.html', context)
